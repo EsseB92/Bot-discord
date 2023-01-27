2ded039bcc64b4a2eef54168d4dbd35f5dd88b84
Bertrand Sudre <sudrebertrand@gmail.com>
2023-01-27 09:50:56 +0100

    fix BitFieldInvalid

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const Discord = require('discord.js');
-const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
+const client = new Discord.Client({ 
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages
+    ] 
+});
 
 client.on('guildMemberAdd', member => {
-  // Replace "welcome" with the name of your welcome channel
-  const welcomeChannel = member.guild.channels.cache.find(channel => channel.name === 'welcome');
-  welcomeChannel.send(`Bienvenue ${member} sur notre serveur!`);
+    // Replace "welcome" with the name of your welcome channel
+    const welcomeChannel = member.guild.channels.cache.find(channel => channel.name === 'welcome');
+    welcomeChannel.send(`Bienvenue ${member} sur notre serveur!`);
 });
 
 client.login(process.env.TOKEN);
