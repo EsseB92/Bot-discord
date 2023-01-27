8afe7a236a0f625275b0e54188b03b3abccbb87c
Bertrand Sudre <sudrebertrand@gmail.com>
2023-01-27 13:02:07 +0100

    suppression de plusieurs logs de test

--- events/guild_messages/messageCreate.js
+++ events/guild_messages/messageCreate.js
@@ -4,28 +4,18 @@ module.exports =  {
     name: 'messageCreate',
     once: false,
     execute(client, message) {
-        if (message.author.bot) {
-            console.log(`Message envoyé par le bot: ${message}`);
-            return;
-        } 
-        if (!message.content.startsWith(prefix)) {
-            console.log(`Le message ne contient pas le prefix(${prefix}): ${message}`);
-            return;
-        }
+        if (message.author.bot) return;
+        if (!message.content.startsWith(prefix)) return;
 
         const args = message.content.slice(prefix.length).trim().split(/ +/g);
-        console.log(`Arguments: ${args}`);
         const cmdName = args.shift().toLowerCase();
-        console.log(`Commandes: ${cmdName}`);
         
         if (cmdName.length == 0) {
             console.log(`Aucune commande existante`);
             return;
         }
         
-        
         let cmd = client.commands.get(cmdName);
-        console.log(`cmd: ${cmd}`);
         if(cmd) cmd.run(client, message, args);
     }
 }
