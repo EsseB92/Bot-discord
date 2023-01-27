a85d4fc2f67d657c01b147b167fd8af9d3df3d99
Bertrand Sudre <sudrebertrand@gmail.com>
2023-01-27 12:34:40 +0100

    test

--- events/guild_messages/messageCreate.js
+++ events/guild_messages/messageCreate.js
@@ -4,14 +4,15 @@ module.exports =  {
     name: 'messageCreate',
     once: false,
     execute(client, message) {
-        console.log("test");
+        
         if (message.author.bot) return;
         if (!message.content.startsWith(prefix)) return;
 
         const args = message.content.slice(prefix.length).trim().split(/ +/g);
         const cmdName = args.shift().toLowerCase();
         if (cmdName.length == 0) return;
-
+        console.log(`cmdName: ${cmdName}`);
+        console.log(`cmd: ${cmd}`);
         let cmd = client.commands.get(cmdName);
         if(cmd) cmd.run(client, message, args);
     }
