1249c360f4935b9779a35db5dddd3f7df0222017
Bertrand Sudre <sudrebertrand@gmail.com>
2023-01-27 13:01:49 +0100

    Log des event et des cmd

--- utils/handlers/CommandUtil.js
+++ utils/handlers/CommandUtil.js
@@ -5,8 +5,8 @@ const pGlob = promisify(glob);
 module.exports = async (client) => {
     (await pGlob(`${process.cwd()}/commands/*/*.js`)).map(async cmdFile => {
         const cmd = require(cmdFile);
-        console.log(cmd.name, cmd);
         client.commands.set(cmd.name, cmd);
 
+        console.log(`Commande chargée: ${cmd.name}`);
     })
 }
