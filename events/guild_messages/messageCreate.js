e3082be3ca79071595e250a8d0aa005aa953382a
Bertrand Sudre <sudrebertrand@gmail.com>
2023-01-27 12:44:44 +0100

    test

--- events/guild_messages/messageCreate.js
+++ events/guild_messages/messageCreate.js
@@ -5,13 +5,25 @@ module.exports =  {
     once: false,
     execute(client, message) {
         
-        if (message.author.bot) return;
-        if (!message.content.startsWith(prefix)) return;
+        if (message.author.bot) {
+            console.log(`Message envoyé par le bot: ${message}`);
+            return;
+        } 
+        if (!message.content.startsWith(prefix)) {
+            console.log(`Le message ne contient pas le prefix(${prefix}): ${message}`);
+            return;
+        }
 
         const args = message.content.slice(prefix.length).trim().split(/ +/g);
+        console.log(`Arguments: ${args}`);
         const cmdName = args.shift().toLowerCase();
-        console.log(`cmdName: ${cmdName}`);
-        if (cmdName.length == 0) return;
+        console.log(`Commandes: ${cmdName}`);
+        
+        if (cmdName.length == 0) {
+            console.log(`Aucune commande existante`);
+            return;
+        }
+        
         
         let cmd = client.commands.get(cmdName);
         console.log(`cmd: ${cmd}`);
