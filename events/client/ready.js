51c7ccd49c53d540552e30e0ee1a38200f0cdbbc
Bertrand Sudre <sudrebertrand@gmail.com>
2023-01-27 15:41:51 +0100

    gestion cmd

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,7 +1,11 @@
 module.exports =  {
     name: 'ready',
     once: true,
-    execute(client) {
+    async execute(client) {
         console.log('Le bot est prêt');
+
+        // Instantané  différent de Global (dure environ 1h)
+        const devGuild = await client.guilds.cache.get('1068467214027604019');
+        devGuild.commands.set(client.commands.map(cmd => cmd));
     }
 }
