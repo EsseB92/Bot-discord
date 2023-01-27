0c524900b5b889b9b0346c1b8c704363831b35a7
Bertrand Sudre <sudrebertrand@gmail.com>
2023-01-27 11:53:47 +0100

    suppression

--- events/guildMemberAdd.js
+++
@@ -1,5 +0,0 @@
-module.exports = (client, member) => {
-    const role = member.guild.roles.cache.find(role => role.name === "Nouveau membre");
-    member.roles.add(role);
-    member.send("Bienvenue sur le serveur ! Nous t'avons attribué le rôle de 'nouveau membre'.");
-}
