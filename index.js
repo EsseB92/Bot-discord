d7edf47a825bce3528eb02664a41ab9e3c6adfb4
Bertrand Sudre <sudrebertrand@gmail.com>
2023-01-27 17:04:53 +0100

    connection BDD mongo

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const { Client, GatewayIntentBits, Collection } = require('discord.js');
 const dotenv = require('dotenv'); dotenv.config();
+const mongoose = require('mongoose');
 const client = new Client({
     intents: [
-        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMembers,
         GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.Guilds,
         GatewayIntentBits.MessageContent
     ]
 });
@@ -16,5 +18,14 @@ process.on('exit', code => { console.log(`Le processus s'est arrêté avec le co
 process.on('uncaughtException', (err, origin) => { console.log(`UNCAUGHT_EXCEPTION: ${err}`, `Origine: ${origin}`) });
 process.on('unhandledRejection', (reason, promise) => { console.log(`UNHANDLED_REJECTION: ${reason}\n-----\n`, promise) });
 process.on('warning', (...args) => console.log(...args));
+
+mongoose.connect(process.env.DATABASE_URI, {
+    autoIndex: false, 
+    maxPoolSize: 10,
+    serverSelectionTimeoutMS: 5000,
+    socketTimeoutMS: 45000,
+    family: 4
+}).then(() => { console.log('Le client est connecté à la base de données!') })
+    .catch(err => { console.log("Erreur: "+err); });
 
 client.login(process.env.TOKEN);
