4fecfe9582019dce0fe29fe44d92650597cddb9d
Bertrand Sudre <sudrebertrand@gmail.com>
2023-01-27 11:33:30 +0100

    réinitialisation

--- index.js
+++ index.js
@@ -1,31 +1,12 @@
-const {Client, GatewayIntentBits} = require('discord.js');
+const { Client, GatewayIntentBits } = require('discord.js');
 const client = new Client({
     intents: [
-        GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildMessages
+        GatewayIntentBits.Guilds
     ] 
 });
-const fs = require('fs');
 
-client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
-});
-
-fs.readdir("./events/", (err, files) => {
-    if (err) return console.error(err);
-    files.forEach(file => {
-      if (!file.endsWith(".js")) return;
-      const event = require(`./events/${file}`);
-      let eventName = file.split(".")[0];
-      client.on(eventName, event.bind(null, client));
-    });
-});
-
-
-client.on('message', msg => {
-    if(msg.content === 'ping') {
-        msg.reply('Pong!');
-    }
+client.once('ready', () => {
+    console.log(`Je suis prêt`);
 });
 
 client.login(process.env.TOKEN);
