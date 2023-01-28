92bf44328e41fed25daf0fb36dd33007417109cd
Bertrand Sudre <sudrebertrand@gmail.com>
2023-01-28 16:42:44 +0100

    plus de dayjs => Date.now()

--- events/guild_members/guildMemberRemove.js
+++ events/guild_members/guildMemberRemove.js
@@ -1,4 +1,3 @@
-const dayjs = require('dayjs');
 const { EmbedBuilder } = require('discord.js');
 
 module.exports =  {
@@ -16,7 +15,7 @@ module.exports =  {
                 ・Nom d'utilisateur: ${member.displayName}
                 ・Créé le: <t:${parseInt(member.user.createdTimestamp / 1000)}:f> (<t:${parseInt(member.user.createdTimestamp / 1000)}:R>)
                 ・Rejoint le: <t:${parseInt(member.joinedTimestamp / 1000)}:f> (<t:${parseInt(member.joinedTimestamp / 1000)}:R>)
-                ・Quitté le: <t:${dayjs().unix()}:f> (<t:${dayjs().unix()}:R>)
+                ・Quitté le: <t:${parseInt(Date.now() / 1000)}:f> (<t:${parseInt(Date.now() / 1000)}:R>)
             `)
             .setTimestamp()
             .setFooter({ text: "L'utilisateur a quitté!"});
