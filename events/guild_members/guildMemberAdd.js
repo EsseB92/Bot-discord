73995abb026cc1f764e5b845fe3fad0daa398ed5
Bertrand Sudre <sudrebertrand@gmail.com>
2023-01-28 03:38:56 +0100

    Tabulation

--- events/guild_members/guildMemberAdd.js
+++ events/guild_members/guildMemberAdd.js
@@ -4,15 +4,20 @@ module.exports =  {
     name: 'guildMemberAdd',
     once: false,
     async execute(client, member) {
+
         const embed = new EmbedBuilder()
-            .setAuthor({ name: `${member.user.tag} (${member.id})`, iconURL: member.user.displayAvatarURL() })
+            .setAuthor({
+                name: `${member.user.tag} (${member.id})`, 
+                iconURL: member.user.displayAvatarURL(),
+            })
             .setColor('#30ffaa')
-            .setDescription(`・Nom d'utilisateur: ${member}
-                             ・Créé le: <t:${parseInt(member.user.createdTimestamp / 1000)}:f> (<t:${parseInt(member.user.createdTimestamp / 1000)}:R>)
-                             ・Rejoint le: <t:${parseInt(member.joinedTimestamp / 1000)}:f> (<t:${parseInt(member.joinedTimestamp / 1000)}:R>) 
+            .setDescription(`
+                ・Nom d'utilisateur: ${member}
+                ・Créé le: <t:${parseInt(member.user.createdTimestamp / 1000)}:f> (<t:${parseInt(member.user.createdTimestamp / 1000)}:R>)
+                ・Rejoint le: <t:${parseInt(member.joinedTimestamp / 1000)}:f> (<t:${parseInt(member.joinedTimestamp / 1000)}:R>)
             `)
             .setTimestamp()
-            .setFooter({ text: 'L\'utilisateur a rejoint!'});
+            .setFooter({ text: "L'utilisateur a rejoint!"});
 
         const logChannel = client.channels.cache.get('1068560374711980032');
         logChannel.send({ embeds: [embed] });
