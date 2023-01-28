6c1c10d7115aa8d39d5aa20657559c0b43ef0b0a
Bertrand Sudre <sudrebertrand@gmail.com>
2023-01-28 18:35:14 +0100

    ajout de userInfo (menu contextuel)

---
+++ commands/users/userinfo.js
@@ -0,0 +1,30 @@
+const { EmbedBuilder } = require('discord.js');
+
+module.exports =  {
+    name: 'userinfo',
+    type: 2,// == 'USER'
+    async runInteraction(client, interaction) {
+        const member = await interaction.guild.members.fetch(interaction.targetId);
+
+        const embed = new EmbedBuilder()
+            .setAuthor({ name: `${member.user.tag} (${member.id})`, iconURL: member.user.bot ? 'https://images.emojiterra.com/google/noto-emoji/v2.034/512px/1f916.png' : 'https://images.emojiterra.com/google/noto-emoji/v2.034/512px/1f9d1.png'}       )
+            .setColor('#8A2BE2')
+            .setImage(member.user.displayAvatarURL())
+            .addFields(
+                { name: 'Nom', value: `${member.displayName}`, inline: true },
+                { name: 'Modérateur', value: `${member.kickable ? '🔴' : '🟢'}`, inline: true },
+                { name: 'Bot', value: `${member.user.bot ? '🟢' : '🔴'}`, inline: true },
+                { name: 'Roles', value: `${member.roles.cache.map(role => role).join(', ').replace(', @everyone', '')}`},
+                { 
+                    name: 'A crée son compte le', 
+                    value: `<t:${parseInt(member.user.createdTimestamp / 1000)}:f> (<t:${parseInt(member.user.createdTimestamp / 1000)}:R>)` 
+                },
+                { 
+                    name: 'A rejoint le serveur le', 
+                    value: `<t:${parseInt(member.joinedTimestamp / 1000)}:f> (<t:${parseInt(member.joinedTimestamp / 1000)}:R>)` 
+                },
+            )
+
+        interaction.reply({ embeds: [embed], ephemeral: true });
+    }
+};
--- utils/handlers/CommandUtil.js
+++ utils/handlers/CommandUtil.js
@@ -5,8 +5,9 @@ const pGlob = promisify(glob);
 module.exports = async (client) => {
     (await pGlob(`${process.cwd()}/commands/*/*.js`)).map(async cmdFile => {
         const cmd = require(cmdFile);
-
-        if(!cmd.name || !cmd.description) return console.log(`----------\nCommande non chargée: pas de nom et/ou de description\nFichier -> ${cmdFile}\n----------`);
+        
+        // 2 => 'USER'
+        if(!cmd.name || (!cmd.description && cmd.type != 2)) return console.log(`----------\nCommande non chargée: pas de nom et/ou de description\nFichier -> ${cmdFile}\n----------`);
 
         client.commands.set(cmd.name, cmd);
         console.log(`Commande chargée: ${cmd.name}`);
