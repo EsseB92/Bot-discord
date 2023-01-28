c9b4db0336f7df638531ac93ce0c068dff1b889d
Bertrand Sudre <sudrebertrand@gmail.com>
2023-01-28 16:43:25 +0100

    ajout de l'event guildcreate et fix ephemeral msg

--- commands/admin/emit.js
+++ commands/admin/emit.js
@@ -1,15 +1,44 @@
 module.exports =  {
     name: 'emit',
     description: 'Emettre un évènement au choix!',
+    options: [
+        {
+            name: "event",
+            description: "Choisir un évènement à émettre",
+            type: 3,
+            required: true,
+            choices: [
+                {
+                    name: 'guildMemberAdd',
+                    value: 'guildMemberAdd',
+                },
+                {
+                    name: 'guildMemberRemove',
+                    value: 'guildMemberRemove',
+                },
+                {
+                    name: 'guildCreate',
+                    value: 'guildCreate',
+                }
+            ],
+        },
+    ],
     runSlash(client, interaction) {
         const eventChoices = interaction.options.getString('event');
 
         if (eventChoices == 'guildMemberAdd') {
             client.emit('guildMemberAdd', interaction.member);
-            interaction.reply({ content: 'Event guildMemberAdd émit!', ephemereal: true });
-        } else {
+            interaction.reply({ content: 'Event guildMemberAdd émit!', ephemeral: true });
+        } 
+        else if(eventChoices == 'guildMemberRemove') {
             client.emit('guildMemberRemove', interaction.member);
-            interaction.reply({ content: 'Event guildMemberRemove émit!', ephemereal: true });
+            interaction.reply({ content: 'Event guildMemberRemove émit!', ephemeral: true });
+        } 
+        else if(eventChoices == 'guildCreate') {
+            client.emit('guildCreate', interaction.guild);
+            interaction.reply({ content: 'Event guildCreate émit!', ephemeral: true });
+        } else {
+            console.log(`${eventChoices} n'est pas valide!`);
         }
     }
 };
