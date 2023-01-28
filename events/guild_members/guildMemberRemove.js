b80f65e6299f42a8193b53e30b8268bc216debc2
Bertrand Sudre <sudrebertrand@gmail.com>
2023-01-28 03:38:20 +0100

    ajout de guildMemberRemove

---
+++ events/guild_members/guildMemberRemove.js
@@ -0,0 +1,27 @@
+const dayjs = require('dayjs');
+const { EmbedBuilder } = require('discord.js');
+
+module.exports =  {
+    name: 'guildMemberRemove',
+    once: false,
+    async execute(client, member) {
+
+        const embed = new EmbedBuilder()
+            .setAuthor({
+                name: `${member.user.tag} (${member.id})`, 
+                iconURL: member.user.displayAvatarURL(),
+            })
+            .setColor('#ff0550')
+            .setDescription(`
+                ・Nom d'utilisateur: ${member.displayName}
+                ・Créé le: <t:${parseInt(member.user.createdTimestamp / 1000)}:f> (<t:${parseInt(member.user.createdTimestamp / 1000)}:R>)
+                ・Rejoint le: <t:${parseInt(member.joinedTimestamp / 1000)}:f> (<t:${parseInt(member.joinedTimestamp / 1000)}:R>)
+                ・Quitté le: <t:${dayjs().unix()}:f> (<t:${dayjs().unix()}:R>)
+            `)
+            .setTimestamp()
+            .setFooter({ text: "L'utilisateur a quitté!"});
+
+        const logChannel = client.channels.cache.get('1068560374711980032');
+        logChannel.send({ embeds: [embed] });
+    },
+};
