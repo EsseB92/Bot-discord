14d9c94da001283cf660a375c7d05daf94176864
Bertrand Sudre <sudrebertrand@gmail.com>
2023-01-30 16:41:12 +0100

    ES6 Ajout du Logger

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-const { Client, GatewayIntentBits, Collection } = require('discord.js');
-const dotenv = require('dotenv'); dotenv.config();
-const mongoose = require('mongoose');
+import { Client, GatewayIntentBits, Collection } from 'discord.js';
+import { config } from 'dotenv'; config();
+import * as mongoose from 'mongoose';
+import CommandUtil from './utils/handlers/CommandUtil.js';
+import EventUtil from './utils/handlers/EventUtil.js';
+import * as Logger from './utils/Logger.js';
 const client = new Client({
     intents: [
         GatewayIntentBits.GuildMembers,
@@ -12,19 +15,28 @@ const client = new Client({
 
 client.commands = new Collection();
 
-['CommandUtil', 'EventUtil'].forEach(handler => { require(`./utils/handlers/${handler}`)(client) });
 
-process.on('exit', code => { console.log(`Le processus s'est arrêté avec le code: ${code}!`) });
-process.on('uncaughtException', (err, origin) => { console.log(`UNCAUGHT_EXCEPTION: ${err}`, `Origine: ${origin}`) });
-process.on('unhandledRejection', (reason, promise) => { console.log(`UNHANDLED_REJECTION: ${reason}\n-----\n`, promise) });
-process.on('warning', (...args) => console.log(...args));
+[CommandUtil, EventUtil].forEach(util => { util(client) });
 
-main().catch(err => console.log(err));
+process.on('exit', code => { Logger.client(`Le processus s'est arrêté avec le code: ${code}!`) });
+process.on('uncaughtException', (err, origin) => { 
+    Logger.error(`UNCAUGHT_EXCEPTION: ${err}`);
+    console.error(`Origine: ${origin}`);
+});
+process.on('unhandledRejection', (reason, promise) => {
+    Logger.warn(`UNHANDLED_REJECTION: ${reason}`);
+    console.log(promise);
+});
+process.on('warning', (...args) => Logger.warn(...args));
 
-async function main(){
-    mongoose.set('strictQuery', false);
-    mongoose.connect(process.env.DATABASE_URI);
-    console.log('Le client est connecté à la base de données!');
-}
+mongoose.set('strictQuery', false);
+mongoose.connect(process.env.DATABASE_URI, {
+    autoIndex: false,
+    maxPoolSize: 10,
+    serverSelectionTimeoutMS: 5000,
+    socketTimeoutMS: 45000,
+    family: 4
+}).then(() => { Logger.client('- Connecté à la base de données'); })
+.catch(err => { Logger.error(err); });
 
 client.login(process.env.TOKEN);
