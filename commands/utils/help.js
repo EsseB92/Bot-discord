73d72e282528df3e416ccfdc4e7730737065d7a1
Bertrand Sudre <sudrebertrand@gmail.com>
2023-01-30 18:17:02 +0100

    Ajout d'ownerOnly, usage et examples Modification de l'embed pour afficher les informations d'une commande

--- commands/utils/help.js
+++ commands/utils/help.js
@@ -2,25 +2,32 @@ import { EmbedBuilder, PermissionFlagsBits, ApplicationCommandOptionType, Permis
 import { readdirSync } from 'fs';
 const commandFolder = readdirSync('./commands');
 
+const contextDescription = {
+    userinfo: 'Renvoie des informations sur l\'utilisateur'
+}
+
 export const name = 'help';
 export const category = 'utils';
-export const permissions = [PermissionFlagsBits.Administrator];
-export const description = 'Commande help';
+export const permissions = [PermissionFlagsBits.SendMessages];
+export const ownerOnly = false;
+export const usage = 'help <commande>';
+export const examples = ['help', 'help ping', 'help poll'];
+export const description = 'Renvoie une liste de commandes filtrée par catégorie';
 export const options = [
     {
-        name: 'command',
+        name: 'commande',
         description: 'Taper le nom de votre commande',
         type: ApplicationCommandOptionType.String,
         required: false
     }
 ];
 export function runInteraction(client, interaction) {
-    const cmdName = interaction.options.getString('command');
+    const cmdName = interaction.options.getString('commande');
 
     if (!cmdName) {
         const noArgsEmbed = new EmbedBuilder()
-            .setColor('#c4108b')
-            .addFields({ name: 'Liste des commandes', value: `Une liste de toutes les catégories disponibles et leurs commandes.\nPour plus d'informations sur une commandes, tapez \`/help <command>\`` });
+            .setColor('#4c00b0')
+            .addFields({ name: 'Liste des commandes', value: `Une liste de toutes les catégories disponibles et leurs commandes.\nPour plus d'informations sur une commandes, tapez \`/help <commande>\`` });
 
         for (const category of commandFolder) {
             noArgsEmbed.addFields({
@@ -36,11 +43,31 @@ export function runInteraction(client, interaction) {
     if (!cmd)
         return interaction.reply({ content: 'cette commande n\'existe pas!', ephemeral: true });
 
+    const description = `${cmd.description ? cmd.description : contextDescription[`${cmd.name}`]}\n\n**Exemples d'utilisation:** /${cmd.examples.join(` | /`)}\n**Permissions:** ${new PermissionsBitField(cmd.permissions).toArray().join(', ')}\n---\n`;
+
+    const footer = `*{} = sous-commande(s) disponible(s) | [] = option(s) obligatoire(s) | <> = option(s) optionnelle(s)\n*Ne pas inclure ces caractères -> {}, [] et <> dans vos commandes.`;
+
     const argsEmbed = new EmbedBuilder()
-        .setColor('#c4108b')
-        .setTitle(`\`${cmd.name}\``)
-        .setDescription(cmd.description)
-        .setFooter({ text: `Permission(s) requise(s): ${new PermissionsBitField(cmd.permissions).toArray().join(', ')}` });
+        .setColor('#4c00b0')
+        .setTitle(`Informations sur la commande ${cmd.name}`)
+        .setDescription(description)
+        .setFooter({ text: footer });
 
     return interaction.reply({ embeds: [argsEmbed], ephemeral: true });
+
+//     return interaction.reply({ content: `
+//         \`\`\`makefile
+// [Help: Commande -> ${cmd.name}] ${cmd.ownerOnly ? '/!\\ Pour les admins du bot uniquement /!\\' : ''}
+
+// ${cmd.description ? cmd.description : contextDescription[`${cmd.name}`]}
+
+// Utilisation: /${cmd.usage}
+// Exemples: /${cmd.examples.join(` | /`)}
+// Permissions: ${new PermissionsBitField(cmd.permissions).toArray().join(', ')}
+
+// ---
+
+// {} = sous-commande(s) disponible(s) | [] = option(s) obligatoire(s) | <> = option(s) optionnelle(s)
+// Ne pas inclure ces caractères -> {}, [] et <> dans vos commandes.
+// \`\`\``, ephemeral: true });
 }
