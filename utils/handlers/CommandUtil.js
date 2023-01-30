e438c395e905fd4682e64d7e6dcf1bd0579c2ed7
Bertrand Sudre <sudrebertrand@gmail.com>
2023-01-30 16:30:08 +0100

    fix ESM user Logger

--- utils/handlers/CommandUtil.js
+++ utils/handlers/CommandUtil.js
@@ -1,25 +1,48 @@
-const { promisify } = require('util');
-const { glob } = require('glob');
-const { PermissionsBitField, ApplicationCommandPermissionType } = require('discord.js');
+import { promisify } from 'util';
+import glob from 'glob';
+import { PermissionsBitField, ApplicationCommandPermissionType } from 'discord.js';
 const pGlob = promisify(glob);
+import * as Logger from '../Logger.js';
+import { pathToFileURL } from 'url';
 
-module.exports = async (client) => {
+export default async (client) => {
     (await pGlob(`${process.cwd()}/commands/*/*.js`)).map(async cmdFile => {
-        const cmd = require(cmdFile);
-        
-        // 2 => 'USER'
-        if(!cmd.name || (!cmd.description && cmd.type != ApplicationCommandPermissionType.User)) return console.log(`----------\nCommande non chargée: pas de nom et/ou de description\nFichier -> ${cmdFile}\n----------`);
+        const cmd = import(pathToFileURL(cmdFile).toString());
 
-        if(!cmd.permissions) return console.log(`----------\nCommande non chargée: pas de permission\nFichier -> ${cmdFile}\n----------`);
 
-        new PermissionsBitField(cmd.permissions).toArray().forEach(permission => {
-            if(!permissionList.includes(permission)) {
-                return console.log(`----------\nCommande non chargé: erreur de typo sur la permission ${permission}\nFichier -> ${cmdFile}\n----------`);
-            }
+        cmd.then(promise => {
+            if(!promise.name) return Logger.warn(`Commande non chargée: Il faut ajouter un nom à votre commande\n                   Fichier -> ${cmdFile}`);
+
+            if(!promise.description && promise.type != ApplicationCommandPermissionType.User) return Logger.warn(`Commande non chargée: Il faut ajouter une description à votre commande\n                   Fichier -> ${cmdFile}`);
+
+            if(!promise.category) return Logger.warn(`Commande non chargée: Il faut ajouter une catégorie à votre commande\n                   Fichier -> ${cmdFile}`);
+
+            if(!promise.permissions) return Logger.warn(`Commande non chargée: Il faut ajouter une/des permissions à votre commande\n                   Fichier -> ${cmdFile}`);
+
+            new PermissionsBitField(promise.permissions).toArray().forEach(permission => {
+                if(!permissionList.includes(permission)) {
+                    return Logger.typo(`Commande non chargée: erreur de typo sur la permission ${permission}\n                   Fichier -> ${cmdFile}`);
+                }
+            });
+
+            client.commands.set(promise.name, promise);
+            Logger.command(`- ${promise.name}`);
         });
+        
+        // if(!cmd.name || (!cmd.description && cmd.type != ApplicationCommandPermissionType.User)) return error(`----------\nCommande non chargée: pas de nom et/ou de description\nFichier -> ${cmdFile}\n----------`);
 
-        client.commands.set(cmd.name, cmd);
-        console.log(`Commande chargée: ${cmd.name}`);
+        // if(!cmd.category) return console.log(`----------\nCommande non chargée: pas de catégorie\nFichier -> ${cmdFile}\n----------`);
+
+        // if(!cmd.permissions) return console.log(`----------\nCommande non chargée: pas de permission\nFichier -> ${cmdFile}\n----------`);
+
+        // new PermissionsBitField(cmd.permissions).toArray().forEach(permission => {
+        //     if(!permissionList.includes(permission)) {
+        //         return console.log(`----------\nCommande non chargé: erreur de typo sur la permission ${permission}\nFichier -> ${cmdFile}\n----------`);
+        //     }
+        // });
+
+        // client.commands.set(cmd.name, cmd);
+        // console.log(`Commande chargée: ${cmd.name}`);
     });
 };
 
--- utils/handlers/EventUtil.js
+++ utils/handlers/EventUtil.js
@@ -1,22 +1,40 @@
-const { promisify } = require('util');
-const { glob } = require('glob');
+import { promisify } from 'util';
+import glob from 'glob';
+import { pathToFileURL } from 'url';
 const pGlob = promisify(glob);
+import * as Logger from '../Logger.js';
 
-module.exports = async (client) => {
+export default async (client) => {
     (await pGlob(`${process.cwd()}/events/*/*.js`)).map(async eventFile => {
-        const event = require(eventFile);
+        const event = import(pathToFileURL(eventFile).toString());
+        
+        event.then(promise => {
+            if(!promise.name) return Logger.warn(`Evènement non déclenché: Il faut ajouter un nom à votre évènement\n                   Fichier -> ${eventFile}`);
 
-        if(!eventList.includes(event.name) || !event.name) {
-            return console.log(`----------\nEvènement non chargé: erreur de typo (ou pas de nom)\nFichier -> ${eventFile}\n----------`);
-        }
+            if(!eventList.includes(promise.name)) return Logger.typo(`Evènement non déclenché: Erreur de typo\n                   Fichier -> ${eventFile}`);
+            
+            
 
-        if (event.once) {
-            client.once(event.name, (...args) => event.execute(client, ...args));
-        } else {
-            client.on(event.name, (...args) => event.execute(client, ...args));
-        }
+            if (promise.once) {
+                client.once(promise.name, (...args) => promise.execute(client, ...args));
+            } else {
+                client.on(promise.name, (...args) => promise.execute(client, ...args));
+            }
+    
+            Logger.event(`- ${promise.name}`);
+        });
 
-        console.log(`Evènement chargé: ${event.name}`);
+        // if(!eventList.includes(event.name) || !event.name) {
+        //     return console.log(`----------\nEvènement non chargé: erreur de typo (ou pas de nom)\nFichier -> ${eventFile}\n----------`);
+        // }
+
+        // if (event.once) {
+        //     client.once(event.name, (...args) => event.execute(client, ...args));
+        // } else {
+        //     client.on(event.name, (...args) => event.execute(client, ...args));
+        // }
+
+        // console.log(`Evènement chargé: ${event.name}`);
     })
 }
 
