e32ac5ed9681d4eca3012f6427a532acf497c6b4
Bertrand Sudre <sudrebertrand@gmail.com>
2023-01-30 16:37:28 +0100

    ES6

--- events/client/interactionCreate.js
+++ events/client/interactionCreate.js
@@ -1,17 +1,20 @@
-const { PermissionsBitField } = require("discord.js");
+import { PermissionsBitField } from "discord.js";
 
 
-module.exports =  {
-    name: 'interactionCreate',
-    once: false,
-    async execute(client, interaction) {
-        if (interaction.isCommand() || interaction.isContextMenu()) {
-            const cmd = client.commands.get(interaction.commandName);
-            if(!cmd) return interaction.reply('Cette commande n\'existe pas!');
+export const name = 'interactionCreate';
+export const once = false;
+export async function execute(client, interaction) {
+    if (interaction.isCommand() || interaction.isContextMenu()) {
+        const cmd = client.commands.get(interaction.commandName);
+        if (!cmd)
+            return interaction.reply('Cette commande n\'existe pas!');
 
-            if (!interaction.member.permissions.has([cmd.permissions])) return interaction.reply({ content: `Vous n'avez pas la/les permission(s) requise(s) (\`${new PermissionsBitField(cmd.permissions).toArray().join(', ')}\`) pour taper cette commande!`, ephemeral: true });
+        if (!interaction.member.permissions.has([cmd.permissions]))
+            return interaction.reply({
+                content: `Vous n'avez pas la/les permission(s) requise(s) (\`${new PermissionsBitField(cmd.permissions).toArray().join(', ')}\`) pour taper cette commande!`,
+                ephemeral: true
+            });
 
-            cmd.runInteraction(client, interaction);
-        }
+        cmd.runInteraction(client, interaction);
     }
 }
