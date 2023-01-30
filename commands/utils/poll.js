5550b9297e4e71bf59f566bbe86d719d6d4d9625
Bertrand Sudre <sudrebertrand@gmail.com>
2023-01-30 16:38:57 +0100

    ES6 Ajout des options

--- commands/utils/poll.js
+++ commands/utils/poll.js
@@ -1,22 +1,35 @@
-const { EmbedBuilder, PermissionFlagsBits } = require('discord.js');
+import { EmbedBuilder, PermissionFlagsBits, ApplicationCommandOptionType } from 'discord.js';
 
-module.exports =  {
-    name: 'poll',
-    permissions: [PermissionFlagsBits.Administrator],
-    description: 'Poster votre propre sondage!',
-    async runInteraction(client, interaction) {
-        const pollTitle = interaction.options.getString('title');
-        const pollContent = interaction.options.getString('content');
-
-        const embed = new EmbedBuilder()
-            .setTitle(pollTitle)
-            .setColor('#1528ff')
-            .setDescription(pollContent)
-            .setTimestamp()
-            .setFooter({ text: `Nouveau sondage généré par ${interaction.user.tag}!` })
-
-        const poll = await interaction.reply({ embeds: [embed], fetchReply: true });
-        poll.react('✅');
-        poll.react('❌');
+export const name = 'poll';
+export const category = 'utils';
+export const permissions = [PermissionFlagsBits.Administrator];
+export const description = 'Poster votre propre sondage!';
+export const options = [
+    {
+        name: 'title',
+        description: 'Taper le titre de votre sondage',
+        type: ApplicationCommandOptionType.String,
+        required: true,
+    },
+    {
+        name: 'content',
+        description: 'Taper la question de votre sondage',
+        type: ApplicationCommandOptionType.String,
+        required: true,
     }
-};
+];
+export async function runInteraction(client, interaction) {
+    const pollTitle = interaction.options.getString('title');
+    const pollContent = interaction.options.getString('content');
+
+    const embed = new EmbedBuilder()
+        .setTitle(pollTitle)
+        .setColor('#1528ff')
+        .setDescription(pollContent)
+        .setTimestamp()
+        .setFooter({ text: `Nouveau sondage généré par ${interaction.user.tag}!` });
+
+    const poll = await interaction.reply({ embeds: [embed], fetchReply: true });
+    poll.react('✅');
+    poll.react('❌');
+}
